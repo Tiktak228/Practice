ced2afc6a8b20c9527d8d505b043da2b850933d6
Tiktak228 <T89173311396@yandex.ru>
2019-06-20 22:16:46 +0400

    Сделал третий номер

---
+++ Number three.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	int a, e, y, u, i, o;
+	a = 0;
+	e = 0;
+	y = 0;
+	u = 0;
+	i = 0;
+	o = 0;
+	getline(cin, str);
+	for (int j = 0; j < str.length(); j++)
+	{
+		if (str[j] == 'A')
+			str[j] = 'a';
+		if (str[j] == 'E')
+			str[j] = 'e';
+		if (str[j] == 'Y')
+			str[j] = 'y';
+		if (str[j] == 'U')
+			str[j] = 'u';
+		if (str[j] == 'I')
+			str[j] = 'i';
+		if (str[j] == 'E')
+			str[j] = 'e';
+		if (str[j] == 'O')
+			str[j] = 'o';
+	}
+	cout << str << endl;
+	system("pause");
+	return 0;
+}
