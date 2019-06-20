00629fc6360a98577e7b8325ba80957eed826d59
Tiktak228 <T89173311396@yandex.ru>
2019-06-20 20:57:29 +0400

    Написал первую программу

---
+++ Number one.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	int a, e, y, u, i, o;
+	a = 0;
+	e = 0; 
+	y = 0; 
+	u = 0; 
+	i = 0; 
+	o = 0; 
+	getline(cin, str);
+		for (int j = 0; j < str.length(); j++)
+	{
+		if (str[j] == 'a')
+			a++;
+		if (str[j] == 'e')
+			e++;
+		if (str[j] == 'y')
+			y++;
+		if (str[j] == 'u')
+			u++;
+		if (str[j] == 'i')
+			i++;
+		if (str[j] == 'o')
+			o++;
+	}
+	cout << "a = " << a << " " << "e = " << e << " " << "y = " << y << " " << "u = " << u << " " << "i = " << i << " " << "o = " << o << endl;
+	system("pause");
+	return 0;
+}
