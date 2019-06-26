6279b2fc6303055923bd4e3464961bb4bbdcf0a1
Tiktak228 <49941257+Tiktak228@users.noreply.github.com>
2019-06-26 16:56:13 +0000

    Исправил

--- NumberThree.cpp
+++ NumberThree.cpp
@@ -7,13 +7,7 @@ using namespace std;
 int main()
 {
 	string str;
-	int a, e, y, u, i, o;
-	a = 0;
-	e = 0;
-	y = 0;
-	u = 0;
-	i = 0;
-	o = 0;
+	int a=0, e=0, y=0, u=0, i=0, o=0;
 	getline(cin, str);
 	for (int j = 0; j < str.length(); j++)
 	{
